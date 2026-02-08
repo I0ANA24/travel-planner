8bf25632fe80fdf9ed4e67d6aaf6b06b852da010
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2026-02-08 13:43:10 +0200

    Adding New Trips

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [{
+      hostname: "gog53pkxdx.ufs.sh",
+    }]
+  }
 };
 
 export default nextConfig;
